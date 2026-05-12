d066432a5736c7b6d57324ba76b9f71a17cd3947
Monika <moni24052706@gmail.com>
2026-05-12 23:53:07 +0530

    updated Project

---
+++ src/main/java/com/devops/demo/service/UserService.java
@@ -0,0 +1,24 @@
+package com.devops.demo.service;
+
+import com.devops.demo.model.User;
+import com.devops.demo.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository repo;
+
+    public UserService(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    public User save(User user) {
+        return repo.save(user);
+    }
+
+    public List<User> getAll() {
+        return repo.findAll();
+    }
+}
